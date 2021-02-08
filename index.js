e61721c791806983ab04c497a3857a9dd6820943
Duyan-Kim <arbolkim@gmail.com>
2021-02-08 21:41:41 +0900

    feat: register 기능 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 3001;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-ulencoded 이렇게 생긴 데이터를 분석해서 가져올 수 있게 해준다
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 이렇게 생긴 데이터를 분석해서 가져올 수 있게 해준다.
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,6 +26,18 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입시 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {

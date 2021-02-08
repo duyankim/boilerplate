274f54250c90c8553ceb306ad7afe3ac5ae2edd3
Duyan-Kim <arbolkim@gmail.com>
2021-02-08 20:18:35 +0900

    feat: MongoDB 연결

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://dbUser:dbUser@boilerplate.hnrfd.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connnected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

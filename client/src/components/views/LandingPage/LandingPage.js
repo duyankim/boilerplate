ec69ca5718ad41f561877a9760fdc562ad39afd5
Duyan-Kim <arbolkim@gmail.com>
2021-02-10 01:09:27 +0900

    feat: react logout 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   });
 
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃에 실패했습니다.");
+      }
+    });
+  };
+
+  return <button onClick={onClickHandler}>로그아웃</button>;
 };
 
 export default LandingPage;
